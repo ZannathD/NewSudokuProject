b945a2f38c258eefe2e4e2fa2e0c009d147a00a8
Night <du5tinl0ckhart555@gmail.com>
2025-04-03 13:23:01 -0400

    timer / check soln / functioned some code

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,18 @@
 //Class:              COP 2006 - CRN 14488
 //Date:               March 25, 2025
 //Description:        A program in C++ that allows a user to play a simple game of Sudoku
-//Log:
+//Log:                4/1 - added background and centered sudoku board on screen and made semi-transparent
+
 
 
 #include <iostream>
-#include <vector>
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <optional>
 #include <unordered_set>
+#include <string>
 
+//Make a header file
 
 //Checking Sudoku board
 bool Solution(int playerBoard[9][9])
@@ -27,6 +29,10 @@ bool Solution(int playerBoard[9][9])
                {
                    return false;
                }
+           }
+           if (playerBoard[row][column] == 0)
+           {
+               return false;
            }
            rowSet.insert(playerBoard[row][column]);
        }
@@ -73,10 +79,98 @@ bool Solution(int playerBoard[9][9])
 }
 
 
+void boundaryFix(int &selectedRow, int &selectedColumn)
+{
+    if (selectedRow == 9 && selectedColumn == 8 || selectedRow == 8 && selectedColumn == 9)
+    {
+        selectedRow = 0;
+        selectedColumn = 0;
+    }
+    else if (selectedRow == -1 && selectedColumn == 0 || selectedRow == 0 && selectedColumn == -1)
+    {
+        selectedRow = 8;
+        selectedColumn = 8;
+    }
+    else if (selectedColumn == -1)
+    {
+        selectedColumn = 8;
+        selectedRow -= 1;
+    }
+    else if (selectedColumn == 9)
+    {
+        selectedColumn = 0;
+        selectedRow += 1;
+    }
+    else if (selectedRow == -1)
+    {
+        selectedRow = 8;
+        selectedColumn -= 1;
+    }
+    else if (selectedRow == 9)
+    {
+        selectedRow = 0;
+        selectedColumn += 1;
+    }
+}
+
+void playerValueInput (int playerBoard[9][9], int selectedRow, int selectedColumn)
+{
+    if (selectedRow != -1 && selectedColumn != -1)
+    {
+        if (isKeyPressed(sf::Keyboard::Key::Num1))
+        {
+            playerBoard[selectedRow][selectedColumn] = 1;
+        }
+        else if (isKeyPressed(sf::Keyboard::Key::Num2))
+        {
+            playerBoard[selectedRow][selectedColumn] = 2;
+        }
+        else if (isKeyPressed(sf::Keyboard::Key::Num3))
+        {
+            playerBoard[selectedRow][selectedColumn] = 3;
+        }
+        else if (isKeyPressed(sf::Keyboard::Key::Num4))
+        {
+            playerBoard[selectedRow][selectedColumn] = 4;
+        }
+        else if (isKeyPressed(sf::Keyboard::Key::Num5))
+        {
+            playerBoard[selectedRow][selectedColumn] = 5;
+        }
+        else if (isKeyPressed(sf::Keyboard::Key::Num6))
+        {
+            playerBoard[selectedRow][selectedColumn] = 6;
+        }
+        else if (isKeyPressed(sf::Keyboard::Key::Num7))
+        {
+            playerBoard[selectedRow][selectedColumn] = 7;
+        }
+        else if (isKeyPressed(sf::Keyboard::Key::Num8))
+        {
+            playerBoard[selectedRow][selectedColumn] = 8;
+        }
+        else if (isKeyPressed(sf::Keyboard::Key::Num9))
+        {
+            playerBoard[selectedRow][selectedColumn] = 9;
+        }
+        else if (isKeyPressed(sf::Keyboard::Key::Num0))
+        {
+            playerBoard[selectedRow][selectedColumn] = 0;
+        }
+    }
+}
+
+
+bool isMouseOver(sf::RectangleShape &button, sf::RenderWindow &window)
+{
+    sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
+    return button.getGlobalBounds().contains(static_cast<sf::Vector2f>(mousePosition));
+}
+
 int main()
 {
-    int column, row;
-    bool keyDownProcessed = false;
+    int column, row, minutes = 0;
+    bool keyDownProcessed = false, complete = false;
     int playerBoard [9][9] =
         {
         {0, 3, 0, 6, 0, 0, 0, 8, 0},
@@ -104,6 +198,7 @@ int main()
         {0, 0, 0, 0, 9, 3, 5, 7, 2}
     };
 
+    //useless basically
     int solnBoard [9][9] =
     {
      {5, 3, 9, 6, 2, 7, 1, 8, 4},
@@ -119,8 +214,7 @@ int main()
 
     //Sudoku Board specifications
     float boxSize = 60.f;
-    sf::Color boxColor(255, 255,255);
-    boxColor.a = 200;
+    sf::Color boxColor(255, 255,255, 200);
     sf::Color outlineColor(0, 0, 0);
 
 
@@ -128,26 +222,34 @@ int main()
     sf::Font font;
     if (!font.openFromFile("../assets/arial.ttf"))
     {
-        std::cerr << "Error loading font!\n";
+        std::cerr << "Error while loading font!\n";
         return -1;
     }
 
-    // Declaring text, and what text to use  -- RENAME -- NUMBERS ON BOARD --
-    sf::Text text(font); //Declare the text object
-    text.setFont(font);
-    text.setCharacterSize(30);
-    text.setStyle(sf::Text::Regular);
 
+    //Check button
+    sf::Text checkButtonText(font, "Check", 30);
+    checkButtonText.setStyle(sf::Text::Bold);
+    checkButtonText.setPosition(sf::Vector2f(1137,885));
+    checkButtonText.setFillColor(sf::Color::Black);
+
+    //Gameplay Background
     sf::Texture background;
     background.loadFromFile("../../assets/background.jpg");
     sf::Sprite sprBackground(background);
 
-    //User Input
+    sf::Texture congrats;
+    congrats.loadFromFile("../../assets/congrats.png");
+    //sf::Image imgCongrats;
+    //imgCongrats = congrats.copyToImage();
+    sf::Sprite sprCongrats(congrats);
+    sprCongrats.setScale(sf::Vector2f(0.47f, 0.5f));
+
+    //User Input default values for grid selection
     int selectedRow = -2, selectedColumn = -2;
 
     //Display beginning
-
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "Sudoku, A C++ Project");
     window.setFramerateLimit(144);
 
 
@@ -160,37 +262,60 @@ int main()
     music.play();
 
 
+    //Start in-game timer
+    sf::Clock clock;
+
     while (window.isOpen())
     {
-
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
             {
                 window.close();
             }
-            //Mouse input for selecting boxes
-            if (isButtonPressed(sf::Mouse::Button::Left))
+        }
+        //Mouse input for selecting boxes
+        if (isButtonPressed(sf::Mouse::Button::Left))
+        {
+            //Get the location of the mouse
+            sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+
+            //Calculate which cell the player is trying to pick
+            selectedRow = (mousePos.y - 270) / boxSize;
+            selectedColumn = (mousePos.x - 690) / boxSize;
+
+            //Verify selected box for testing
+            if (selectedColumn >= 0 && selectedColumn < 9 && selectedRow >= 0 && selectedRow <9)
             {
-                sf::Vector2i mousePos = sf::Mouse::getPosition(window);
-                selectedRow = (mousePos.y - 270) / boxSize;
-                selectedColumn = (mousePos.x - 690) / boxSize;
-                if (selectedColumn >= 0 && selectedColumn < 9 && selectedRow >= 0 && selectedRow <9)
-                {
-                    //Checking what box was clicked
-                    std::cout << "Selected cell: (" << selectedRow << ", " << selectedColumn << ")" << std::endl;
-                }
-                else
-                {
-                    //Default selected box/row so hitting numbers before clicking doesn't change anything
-                    selectedColumn =  -2;
-                    selectedRow = -2;
-                }
+                //Checking what box was clicked
+                std::cout << "Selected cell: (" << selectedRow << ", " << selectedColumn << ")" << std::endl;
+            }
+            else
+            {
+                //Default selected box/row so hitting numbers before clicking doesn't change anything
+                selectedColumn =  -2;
+                selectedRow = -2;
             }
         }
 
+        //Timer
+        sf::Time timer = clock.getElapsedTime();
+        int seconds = timer.asSeconds();
+        if (seconds > 59)
+        {
+            seconds = 0;
+            minutes += 1;
+            clock.restart();
+        }
+
+        std::string full_Timer_String = std::to_string(minutes) + ":" + (seconds < 10 ? "0" : "") + std::to_string(seconds);
+        sf::Text textTimer(font, full_Timer_String, 30);
+        textTimer.setStyle(sf::Text::Bold);
+        textTimer.setFillColor(sf::Color::Black);
+        textTimer.setPosition(sf::Vector2f(937, 885));
 
 
+        //Arrow keys to navigate board
         if (!keyDownProcessed)
         {
             if (isKeyPressed(sf::Keyboard::Key::Down))
@@ -220,37 +345,10 @@ int main()
         }
 
         //Keeping selected Row / Column in the bounds of the board
-        if (selectedRow == 9 && selectedColumn == 8 || selectedRow == 8 && selectedColumn == 9)
-        {
-            selectedRow = 0;
-            selectedColumn = 0;
-        }
-        else if (selectedRow == -1 && selectedColumn == 0 || selectedRow == 0 && selectedColumn == -1)
-        {
-            selectedRow = 8;
-            selectedColumn = 8;
-        }
-        else if (selectedColumn == -1)
-        {
-            selectedColumn = 8;
-            selectedRow -= 1;
-        }
-        else if (selectedColumn == 9)
-        {
-            selectedColumn = 0;
-            selectedRow += 1;
-        }
-        else if (selectedRow == -1)
-        {
-            selectedRow = 8;
-            selectedColumn -= 1;
-        }
-        else if (selectedRow == 9)
-        {
-            selectedRow = 0;
-            selectedColumn += 1;
-        }
+        boundaryFix(selectedRow, selectedColumn);
 
+        //Keyboard input for user to edit board
+        playerValueInput (playerBoard, selectedRow, selectedColumn);
 
 
         //Reset stop for triggering event multiple times
@@ -260,51 +358,6 @@ int main()
                    !isKeyPressed(sf::Keyboard::Key::Right))
         {
             keyDownProcessed = false;  // Reset when no arrow key is pressed
-        }
-
-        //Keyboard input for user to edit board
-        if (selectedRow != -1 && selectedColumn != -1)
-        {
-            if (isKeyPressed(sf::Keyboard::Key::Num1))
-            {
-                playerBoard[selectedRow][selectedColumn] = 1;
-            }
-            else if (isKeyPressed(sf::Keyboard::Key::Num2))
-            {
-                playerBoard[selectedRow][selectedColumn] = 2;
-            }
-            else if (isKeyPressed(sf::Keyboard::Key::Num3))
-            {
-                playerBoard[selectedRow][selectedColumn] = 3;
-            }
-            else if (isKeyPressed(sf::Keyboard::Key::Num4))
-            {
-                playerBoard[selectedRow][selectedColumn] = 4;
-            }
-            else if (isKeyPressed(sf::Keyboard::Key::Num5))
-            {
-                playerBoard[selectedRow][selectedColumn] = 5;
-            }
-            else if (isKeyPressed(sf::Keyboard::Key::Num6))
-            {
-                playerBoard[selectedRow][selectedColumn] = 6;
-            }
-            else if (isKeyPressed(sf::Keyboard::Key::Num7))
-            {
-                playerBoard[selectedRow][selectedColumn] = 7;
-            }
-            else if (isKeyPressed(sf::Keyboard::Key::Num8))
-            {
-                playerBoard[selectedRow][selectedColumn] = 8;
-            }
-            else if (isKeyPressed(sf::Keyboard::Key::Num9))
-            {
-                playerBoard[selectedRow][selectedColumn] = 9;
-            }
-            else if (isKeyPressed(sf::Keyboard::Key::Num0))
-            {
-                playerBoard[selectedRow][selectedColumn] = 0;
-            }
         }
 
 
@@ -321,6 +374,7 @@ int main()
                 sf::RectangleShape box(sf::Vector2f(boxSize, boxSize));
                 box.setPosition(sf::Vector2f(690 + column * boxSize, 270 + row * boxSize));
                 box.setFillColor(boxColor);
+                //Highlighting selected cell for clarity
                 if (column == selectedColumn && row == selectedRow)
                 {
                     box.setFillColor(sf::Color::Yellow);
@@ -329,10 +383,8 @@ int main()
                 box.setOutlineColor(outlineColor);
                 window.draw(box);
 
-                }
             }
-
-
+        }
         //Filling out table with initial table and user table
         for (column = 0; column < 9; column++)
         {
@@ -340,26 +392,50 @@ int main()
             {
                 if (playerBoard[row][column] != 0 && initialBoard[row][column] == 0)
                 {
-                    sf::Text text(font, std::to_string(playerBoard[row][column]), 30);
-                    text.setPosition(sf::Vector2f(690 + column * boxSize + 20, 270 + row * boxSize + 15));
-                    text.setFillColor(sf::Color::Blue);
-                    window.draw(text);
+                    sf::Text playerNum(font, std::to_string(playerBoard[row][column]), 30);
+                    playerNum.setPosition(sf::Vector2f(690 + column * boxSize + 20, 270 + row * boxSize + 15));
+                    playerNum.setFillColor(sf::Color::Blue);
+                    window.draw(playerNum);
                 }
                 if (initialBoard[row][column] != 0)
                 {
-                   // std::cout << "Text position: (" << column * boxSize + 20 << ", " << row * boxSize + 15 << ")" << std::endl;
-                    sf::Text text(font, std::to_string(initialBoard[row][column]), 30);
-                    text.setPosition(sf::Vector2f(690 + column * boxSize + 20, 270 + row * boxSize + 15));
-                    text.setFillColor(sf::Color::Black);
-                    window.draw(text);
+                    // std::cout << "Text position: (" << column * boxSize + 20 << ", " << row * boxSize + 15 << ")" << std::endl;
+                    sf::Text initialNum(font, std::to_string(initialBoard[row][column]), 30);
+                    initialNum.setPosition(sf::Vector2f(690 + column * boxSize + 20, 270 + row * boxSize + 15));
+                    initialNum.setFillColor(sf::Color::Black);
+                    window.draw(initialNum);
                 }
             }
+        }
+        //Timer display
+        sf::RectangleShape timerBox(sf::Vector2f(100, 50));
+        timerBox.setPosition(sf::Vector2f(910, 880));
+        timerBox.setFillColor(sf::Color::White);
+        timerBox.setOutlineColor(sf::Color::Black);
+        timerBox.setOutlineThickness(2.f);
+        window.draw(timerBox);
+        window.draw(textTimer);
+
+        //Check button
+        sf::RectangleShape checkButton(sf::Vector2f(100,50));
+        checkButton.setPosition(sf::Vector2f(1130,880));
+        checkButton.setFillColor(sf::Color::Green);
+        checkButton.setOutlineColor(sf::Color::Black);
+        checkButton.setOutlineThickness(2.f);
+        window.draw(checkButton);
+        window.draw(checkButtonText);
 
 
+        if (isButtonPressed(sf::Mouse::Button::Left) && (isMouseOver(checkButton, window)) && Solution(playerBoard) == 1)
+        {
+            complete = true;
+        }
+        if (complete == true)
+        {
+            window.draw(sprCongrats);
         }
 
         window.display();
     }
-    std::cout << Solution(playerBoard) << std::endl;
 }
 
