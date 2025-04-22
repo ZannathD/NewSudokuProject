34b68dd5ef188cc2c3aa57d180fe6402541bf431
ZannathD <du5tinl0ckhart555@gmail.com>
2025-04-22 11:16:08 -0400

    Delete cmake-build-debug directory

--- cmake-build-debug/_deps/flac-build/microbench/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/steph/NewSudokuProject/cmake-build-debug/_deps/flac-src/microbench
-# Build directory: C:/Users/steph/NewSudokuProject/cmake-build-debug/_deps/flac-build/microbench
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-debug/_deps/flac-build/src/libFLAC/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/steph/NewSudokuProject/cmake-build-debug/_deps/flac-src/src/libFLAC
-# Build directory: C:/Users/steph/NewSudokuProject/cmake-build-debug/_deps/flac-build/src/libFLAC
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
